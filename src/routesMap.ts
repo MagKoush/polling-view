46756c112246fef624db100f092954e001666290
arashk70 <arash.koush@gmail.com>
2020-04-30 19:51:52 -0700

    routesMap

--- src/routesMap.ts
+++ src/routesMap.ts
@@ -4,6 +4,7 @@ export const Components = {
   ELECTION: 'Election',
   HOME: 'Home',
   [NOT_FOUND]: 'NotFound',
+  RESULTS: 'Results',
   SIGN_IN: 'SignIn',
   SIGN_UP: 'SignUp',
 };
@@ -11,6 +12,7 @@ export const Components = {
 export default {
   ELECTION: '/election',
   HOME: '/',
+  RESULTS: '/results',
   SIGN_IN: '/signin',
   SIGN_UP: '/singup',
 };
