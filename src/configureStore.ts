b3a0e2b7c4d3de4053f46f89e668056e604e9a64
arashk70 <arash.koush@gmail.com>
2020-04-30 19:51:39 -0700

    ConfigureStore Configuration

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -2,17 +2,17 @@ import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import { connectRoutes } from 'redux-first-router';
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 
-import { fetchElection } from './epics';
+import { getElectionByUserEpic, getUserEpic, getVotesByElectionEpic, postVotesEpic } from './epics';
 import { State } from './interfaces';
-import { page, user } from './reducers';
+import { election, page, user, votes } from './reducers';
 import RoutesMap from './routesMap';
 
 export default function configureStore(preloadedState: State): any {
   const { reducer, middleware, enhancer } = connectRoutes(RoutesMap);
 
-  const rootEpic = combineEpics(fetchElection);
+  const rootEpic = combineEpics(getElectionByUserEpic, getUserEpic, getVotesByElectionEpic, postVotesEpic);
   const epicMiddleware = createEpicMiddleware();
-  const rootReducer = combineReducers({ location: reducer, page, user });
+  const rootReducer = combineReducers({ election, location: reducer, page, user, votes });
   const middlewares = applyMiddleware(epicMiddleware, middleware);
   const composeEnhancers = (window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose) || compose;
   const enhancers = composeEnhancers(enhancer, middlewares);
